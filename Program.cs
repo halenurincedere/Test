d5b278ccabadf3f2a1529944e45e9396400a7089
Halenur İncedere <halenurincedere@gmail.com>
2024-12-20 11:15:43 +0300

    MarketApp

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace Test
+class Program
 {
-    class Program
-    {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        Console.WriteLine ("Gün sayısı:");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+        switch (dayNumber)
+        {
+            case 1:
+            Console.WriteLine("Pazartesi");
+            break;
+
+            case 2:
+            Console.WriteLine("Salı");
+            break;
+
+            case 3:
+            Console.WriteLine("Çarşamba");
+            break;
+
+            case 4:
+            Console.WriteLine("Perşembe");
+            break;
+        }
     }
     }
-}
--- obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8d13207b23da826023b546421bc374614f29d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be0359d26c88d075dfb553f7f1e624b192278578")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
